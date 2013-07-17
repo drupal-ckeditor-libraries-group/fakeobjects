7bd108ac6d408349b467a5497ea7072e47005de3
Wiktor Walc <w.walc@cksource.com>
2013-07-17 17:38:41 +0200

    Updated translations, added Indonesian language.

--- lang/id.js
+++ lang/id.js
@@ -2,10 +2,10 @@
 Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
-CKEDITOR.plugins.setLang( 'fakeobjects', 'ko', {
-	anchor: '책갈피 삽입/변경',
+CKEDITOR.plugins.setLang( 'fakeobjects', 'id', {
+	anchor: 'Anchor', // MISSING
 	flash: 'Flash Animation', // MISSING
-	hiddenfield: '숨김필드',
-	iframe: 'IFrame',
+	hiddenfield: 'Kolom Tersembunyi',
+	iframe: 'IFrame', // MISSING
 	unknown: 'Unknown Object' // MISSING
 });
